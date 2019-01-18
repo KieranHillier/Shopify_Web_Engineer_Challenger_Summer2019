b1383f5ef1cef11fc7e5eb6514a2d6e20a4fda21
Kieran Hillier <kieran@Kierans-MacBook-Pro.local>
2019-01-18 13:33:15 -0500

    Updates

---
+++ precache-manifest.1ca492c4746f4c6013d7e07336a0b20c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90d8098dab7b9094e0c0",
+    "url": "/Shopify_Web_Engineer_Challenger_Summer2019/static/css/main.1c7505fe.chunk.css"
+  },
+  {
+    "revision": "90d8098dab7b9094e0c0",
+    "url": "/Shopify_Web_Engineer_Challenger_Summer2019/static/js/main.90d8098d.chunk.js"
+  },
+  {
+    "revision": "9240cc84ab4ec54b3a62",
+    "url": "/Shopify_Web_Engineer_Challenger_Summer2019/static/js/1.9240cc84.chunk.js"
+  },
+  {
+    "revision": "572b715c0d0f5261a82d",
+    "url": "/Shopify_Web_Engineer_Challenger_Summer2019/static/js/runtime~main.572b715c.js"
+  },
+  {
+    "revision": "9c876e827217398231971a9c86a282f4",
+    "url": "/Shopify_Web_Engineer_Challenger_Summer2019/index.html"
+  }
+];
